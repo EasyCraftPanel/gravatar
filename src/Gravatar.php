1cb6cd0c465c12035cc509291b1a78299e39575b
kengwang <atkengwang@qq.com>
2021-08-23 22:12:55 +0800

    Change Base Url

--- src/Gravatar.php
+++ src/Gravatar.php
@@ -22,7 +22,7 @@ class Gravatar
      *
      * @var string
      */
-    private string $publicBaseUrl = 'https://www.gravatar.com/avatar/';
+    private string $publicBaseUrl = 'https://cn.gravatar.com/avatar/';
 
     /**
      * Email address to check.
